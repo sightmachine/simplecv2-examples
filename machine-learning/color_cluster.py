2b4cbc40c7edfe87a0f82f04407e58bee11aa742
Kirill Rozin <krozin@gmail.com>
2014-11-28 17:09:52 +0300

    rework machine learning examples

--- machine-learning/color_cluster.py
+++ machine-learning/color_cluster.py
@@ -1,21 +1,21 @@
-'''
+"""
 This program trys to extract the color pallette from an image
 it could be used in machine learning as a color classifier
-'''
+"""
 print __doc__
 
-from simplecv import *
-disp = Display((640,528))
+from simplecv.api import Camera
+
 cam = Camera()
 count = 0
 pal = None
-while disp.isNotDone():
-    img = cam.getImage()
-    if count%10 == 0:
-        temp = img.scale(.3)
+while True:
+    img = cam.get_image()
+    if count % 10 == 0:
+        temp = img.scale(0.3)
         p = temp.get_palette()
-        pal = temp.draw_palette_colors(size=(640,48))
+        pal = temp.draw_palette_colors(size=(640, 48))
     result = img.re_palette(p)
-    result = result.side_by_side(pal,side='bottom')
-    result.save(disp)
+    result = result.side_by_side(pal, side='bottom')
+    result.show()
     count = count + 1
--- machine-learning/machine_learning_nuts_vs_bolts.py
+++ machine-learning/machine_learning_nuts_vs_bolts.py
@@ -1,4 +1,4 @@
-'''
+"""
 This Example uses scikits-learn to do a binary classfication of images
 of nuts vs. bolts.  Only the area, height, and width are used to classify
 the actual images but data is extracted from the images using blobs.
@@ -8,36 +8,37 @@ meant to give an introductory example for using machine learning
 
 The data set should auto download, if not you can get it from:
 https://github.com/downloads/sightmachine/SimpleCV/nuts_bolts.zip
-'''
+"""
 print __doc__
-from simplecv import *
+
+from simplecv.api import ImageSet, Blob
+from simplecv.base import download_and_extract
 from sklearn.svm import LinearSVC
 from sklearn.linear_model import LogisticRegression
 import numpy as np
 
-#Download the dataset
+# Download the dataset
 machine_learning_data_set = 'https://github.com/downloads/sightmachine/SimpleCV/nuts_bolts.zip'
 data_path = download_and_extract(machine_learning_data_set)
 print 'Test Images Downloaded at:', data_path
 
-display = Display((800,600)) #Display to show the images
 target_names = ['bolt', 'nut']
 
 print 'Loading Bolts for Training'
-bolts = ImageSet(data_path + '/data/supervised/bolts') #Load Bolts for training
-bolt_blobs = [b.find_blobs()[0] for b in bolts] #exact the blobs for our features
-tmp_data = [] #array to store data features
-tmp_target = [] #array to store targets
+bolts = ImageSet(data_path + '/data/supervised/bolts')  # Load Bolts for training
+bolt_blobs = [b.find(Blob)[0] for b in bolts]  # exact the blobs for our features
+tmp_data = []  # array to store data features
+tmp_target = []  # array to store targets
 
-for b in bolt_blobs: #Format Data for SVM
-    tmp_data.append([b.get_area(), b.get_height(), b.get_width()])
+for b in bolt_blobs:  # Format Data for SVM
+    tmp_data.append([b.area, b.height, b.width])
     tmp_target.append(0)
 
 print 'Loading Nuts for Training'
 nuts = ImageSet(data_path + '/data/supervised/nuts')
-nut_blobs = [n.invert().find_blobs()[0] for n in nuts]
+nut_blobs = [n.invert().find(Blob)[0] for n in nuts]
 for n in nut_blobs:
-    tmp_data.append([n.get_area(), n.get_height(), n.get_width()])
+    tmp_data.append([n.area, n.height, n.width])
     tmp_target.append(1)
 
 dataset = np.array(tmp_data)
@@ -50,24 +51,24 @@ clf2 = LogisticRegression().fit(dataset, targets)
 
 print 'Running prediction on bolts now'
 untrained_bolts = ImageSet(data_path + '/data/unsupervised/bolts')
-unbolt_blobs = [b.find_blobs()[0] for b in untrained_bolts]
+unbolt_blobs = [b.find(Blob)[0] for b in untrained_bolts]
 for b in unbolt_blobs:
-    ary = [b.get_area(), b.get_height(), b.get_width()]
+    ary = [b.area, b.height, b.width]
     name = target_names[clf.predict(ary)[0]]
     probability = clf2.predict_proba(ary)[0]
     img = b.image
-    img.draw_text(name)
-    img.save(display)
-    print "Predicted:",name,", Guess:",probability[0], target_names[0],",", probability[1], target_names[1]
+    img.dl().text(name, (10, 10))
+    img.show()
+    print "Predicted:", name, ", Guess:", probability[0], target_names[0], ",", probability[1], target_names[1]
 
 print 'Running prediction on nuts now'
 untrained_nuts = ImageSet(data_path + '/data/unsupervised/nuts')
-unnut_blobs = [n.invert().find_blobs()[0] for n in untrained_nuts]
+unnut_blobs = [n.invert().find(Blob)[0] for n in untrained_nuts]
 for n in unnut_blobs:
-    ary = [n.get_area(), n.get_height(), n.get_width()]
+    ary = [n.area, n.height, n.width]
     name = target_names[clf.predict(ary)[0]]
     probability = clf2.predict_proba(ary)[0]
     img = n.image
-    img.draw_text(name)
-    img.save(display)
-    print "Predicted:",name,", Guess:",probability[0], target_names[0],",", probability[1], target_names[1]
+    img.dl().text(name, (10, 10))
+    img.show()
+    print "Predicted:", name, ", Guess:", probability[0], target_names[0], ",", probability[1], target_names[1]
